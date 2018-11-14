3ff273a4093ddb2825fa2f5d3d414c6a05f1fb10
Ji Wang <traceyw@vt.edu>
2018-11-14 15:32:42 -0500

    implemented show fucntionality for porfolio module

--- app/controllers/portfolios_controller.rb
+++ app/controllers/portfolios_controller.rb
@@ -30,4 +30,7 @@ class PortfoliosController < ApplicationController
   def edit
   	@portfolio_item=Portfolio.find(params[:id])
   end
+  def show
+    @portfolio_item=Portfolio.find(params[:id])
+  end
 end
